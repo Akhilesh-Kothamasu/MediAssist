544bddcb7001dd683b82d0cbe281a1550d657ef3
Akhilesh <akhilesh.kothamasu@musquaretech.com>
2026-04-18 18:38:15 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MediAssist",
+    version="0.1.0",
+    author="Akhilesh",
+    author_email="kothamasuakhilesh83@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
